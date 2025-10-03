6e46117118157823829ec9f1b1514d244d0932d4
Pigeonlover <bravefoot@outlook.com>
2025-10-03 17:41:03 +0100

    Posts page and dynamic page for each post.

--- src/app/posts/[postId]/page.js
+++ src/app/posts/[postId]/page.js
@@ -1,1 +1,23 @@
 //
+import { db } from "@/utils/dbConnection";
+
+export default async function PostIdPage(params) {
+  const postId = params;
+  const post = (
+    await db.query(
+      `SELECT id, title, author, content, created_at FROM pigeonblogposts WHERE id = $1`,
+      [postId]
+    )
+  ).rows[0];
+
+  if (!post) return <p>Post not found!</p>;
+  return (
+    <>
+      <div>
+        <h1>{post.title}</h1>
+        <h2>Posted on: {post.created_at}</h2>
+        <p>{post.content}</p>
+      </div>
+    </>
+  );
+}
--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,11 +1,28 @@
 //
+import Link from "next/link";
+import { db } from "@/utils/dbConnection";
 
-export default function PostsPage() {
+export default async function PostsPage() {
+  const posts = (
+    await db.query(
+      `SELECT id, title, author, content, created_at FROM pigeonblogposts`
+    )
+  ).rows;
+
+  console.log(posts);
+
   return (
     <>
       <div>
         <h1>Posts</h1>
         <p>List of all the posts.</p>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Link href={`/posts/${post.id}`}>{post.title}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
